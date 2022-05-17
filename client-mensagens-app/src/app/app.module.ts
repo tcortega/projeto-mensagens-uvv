97d31db07f40be75e2c81359c32e391918696246
tcortega <tcortega@mail.com>
2022-05-16 21:02:20 -0300

    I don't even know in which stage we are at

--- client-mensagens-app/src/app/app.module.ts
+++ client-mensagens-app/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/pages/login/login.component';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
+import { NavbarComponent } from './auth/components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SignupComponent } from './auth/pages/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxNavbarModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
